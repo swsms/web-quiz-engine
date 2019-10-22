b03656f9c273db5f6ebf00fd184d55e238ee270a
artyom <avburylov@gmail.com>
2019-10-22 21:33:53 +0500

    Create basic project structure

---
+++ src/main/java/org/hyperskill/webquizengine/WebQuizEngineApplication.java
@@ -0,0 +1,13 @@
+package org.hyperskill.webquizengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuizEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQuizEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hyperskill/webquizengine/WebQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyperskill.webquizengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
