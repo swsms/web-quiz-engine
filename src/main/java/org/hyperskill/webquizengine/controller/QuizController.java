e04bdd644b8525e6cd5eaf6276eccf5716d9c5a7
artyom <avburylov@gmail.com>
2019-10-29 17:59:07 +0500

    Use paging properties

--- src/main/java/org/hyperskill/webquizengine/controller/QuizController.java
+++ src/main/java/org/hyperskill/webquizengine/controller/QuizController.java
@@ -2,19 +2,20 @@ package org.hyperskill.webquizengine.controller;
 
 import org.hyperskill.webquizengine.dto.QuizDto;
 import org.hyperskill.webquizengine.dto.ResultDto;
+import org.hyperskill.webquizengine.repository.QuizRepository;
 import org.hyperskill.webquizengine.service.QuizService;
 import org.hyperskill.webquizengine.util.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.hyperskill.webquizengine.util.Utils.checkAnswerOptions;
 import static org.hyperskill.webquizengine.util.Utils.convertQuizEntityToDtoWithoutAnswer;
@@ -25,6 +26,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class QuizController {
     private final Logger logger = LoggerFactory.getLogger(QuizController.class);
     private final QuizService service;
+
+    @Autowired
+    private QuizRepository quizRepository;
 
     @Autowired
     public QuizController(QuizService service) {
@@ -53,18 +57,17 @@ public class QuizController {
         return convertQuizEntityToDtoWithoutAnswer(service.findById(id));
     }
 
-    @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public List<QuizDto> getQuizList() {
-        return service.findAllSortedById().stream()
-                .map(Utils::convertQuizEntityToDtoWithoutAnswer)
-                .collect(Collectors.toList());
-    }
-
     @DeleteMapping(path = "/{id}", produces = APPLICATION_JSON_VALUE)
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void deleteQuiz(@PathVariable long id,
                            @Autowired Principal principal) {
         logger.info("User {} wants to delete a quiz with id {}", principal.getName(), id);
         service.delete(id, principal.getName());
+    }
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public Page<QuizDto> getQuizPage(Pageable pageable) {
+        return quizRepository.findAll(pageable)
+                .map(Utils::convertQuizEntityToDtoWithoutAnswer);
     }
 }
--- src/main/java/org/hyperskill/webquizengine/repository/QuizRepository.java
+++ src/main/java/org/hyperskill/webquizengine/repository/QuizRepository.java
@@ -1,6 +1,6 @@
 package org.hyperskill.webquizengine.repository;
 
 import org.hyperskill.webquizengine.model.Quiz;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface QuizRepository extends CrudRepository<Quiz, Long> { }
+public interface QuizRepository extends PagingAndSortingRepository<Quiz, Long> { }
